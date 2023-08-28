b41455ae018348eb9021efd72cdc6135e52a4ec3
AAAAAAAAAA <88826851+UniqueName12345@users.noreply.github.com>
2023-08-28 17:21:18 +0800

    Add brand-new function (without unit test though :/)

--- pyirl.py
+++ pyirl.py
@@ -7,6 +7,7 @@ __author__ = "DifferentDance8"
 __license__ = "MIT"
 __copyright__ = "Copyright 2023 DifferentDance8"
 
+
 def boolean_input(message, default=False):
     """
     Prompts the user for a boolean input and returns the corresponding boolean value.
@@ -25,4 +26,23 @@ def boolean_input(message, default=False):
         elif value.lower() == "n":
             return False
         elif not value and default:
+            return default
+
+
+def input_with_default(message, default=""):
+    """
+    Prompts the user for input and returns the input value or the default value if the user does not provide any input.
+
+    Parameters:
+        message (str): The message to display to the user when prompting for input.
+        default (str, optional): The default value to return if the user does not provide any input. Defaults to "".
+
+    Returns:
+        str: The input value or the default value if the user does not provide any input.
+    """
+    while True:
+        value = input(message)
+        if value:
+            return value
+        elif default:
             return default
