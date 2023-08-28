de44b1e4d40e6caf25b537aaa64c729164e11c4d
AAAAAAAAAA <88826851+UniqueName12345@users.noreply.github.com>
2023-08-28 17:10:33 +0800

    add library

---
+++ pyirl.py
@@ -0,0 +1,28 @@
+"""
+PYIRL - Python Input and Read Library
+"""
+
+__version__ = "0.0.1"
+__author__ = "DifferentDance8"
+__license__ = "MIT"
+__copyright__ = "Copyright 2023 DifferentDance8"
+
+def boolean_input(message, default=False):
+    """
+    Prompts the user for a boolean input and returns the corresponding boolean value.
+
+    Parameters:
+        message (str): The message to display to the user when prompting for input.
+        default (bool, optional): The default value to return if the user does not provide any input. Defaults to False.
+
+    Returns:
+        bool: True if the user inputs 'y', False if the user inputs 'n', and the default value if the user does not provide any input and a default value is specified.
+    """
+    while True:
+        value = input(message)
+        if value.lower() == "y":
+            return True
+        elif value.lower() == "n":
+            return False
+        elif not value and default:
+            return default
---
+++ unittests.py
@@ -0,0 +1,23 @@
+import unittest
+from unittest.mock import patch
+import pyirl
+
+class TestBooleanInput(unittest.TestCase):
+    
+    @patch('builtins.input', return_value='y')
+    def test_boolean_input_returns_true_for_y(self, mock_input):
+        result = pyirl.boolean_input("Enter 'y': ")
+        self.assertTrue(result)
+        
+    @patch('builtins.input', return_value='n')
+    def test_boolean_input_returns_false_for_n(self, mock_input):
+        result = pyirl.boolean_input("Enter 'n': ")
+        self.assertFalse(result)
+        
+    @patch('builtins.input', return_value='')
+    def test_boolean_input_returns_default_value(self, mock_input):
+        result = pyirl.boolean_input("Enter nothing: ", default=True)
+        self.assertTrue(result)
+
+if __name__ == '__main__':
+    unittest.main()
